b1e6343289780a17159c78be5b504c41dab12722
Namwan Apinya <j_apinya@kkumail.com>
2017-07-11 14:22:46 +0700

    Second Commit -- create xml file 'activity_login.xml' and java files 'LoginActivity.java', 'FirebaseAuth.java'

---
+++ app/src/main/java/com/example/namwan/firebaseauthfacebooklogin/FirebaseAuth.java
@@ -0,0 +1,8 @@
+package com.example.namwan.firebaseauthfacebooklogin;
+
+/**
+ * Created by Namwan on 7/11/2017.
+ */
+
+public class FirebaseAuth {
+}
---
+++ app/src/main/java/com/example/namwan/firebaseauthfacebooklogin/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.namwan.firebaseauthfacebooklogin;
+
+/**
+ * Created by Namwan on 7/11/2017.
+ */
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity {
+}
