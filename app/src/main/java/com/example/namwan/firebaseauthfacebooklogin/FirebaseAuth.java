203322929e9d5adbea88dc3a1389198845208b00
Namwan Apinya <j_apinya@kkumail.com>
2017-07-11 15:19:37 +0700

    Fourth Commit -- add code in MainActivity.java and FirebaseAuth.java

--- app/src/main/java/com/example/namwan/firebaseauthfacebooklogin/FirebaseAuth.java
+++ app/src/main/java/com/example/namwan/firebaseauthfacebooklogin/FirebaseAuth.java
@@ -1,8 +1,20 @@
 package com.example.namwan.firebaseauthfacebooklogin;
 
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 /**
  * Created by Namwan on 7/11/2017.
  */
 
-public class FirebaseAuth {
+public class FirebaseAuth extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
 }
