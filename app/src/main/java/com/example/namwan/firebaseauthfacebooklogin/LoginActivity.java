65542070e61ad04c61f48722f34729b03ec99efd
Namwan Apinya <j_apinya@kkumail.com>
2017-07-13 13:11:55 +0700

    Edit -- get Facebook user's picture to show in application

--- app/src/main/java/com/example/namwan/firebaseauthfacebooklogin/LoginActivity.java
+++ app/src/main/java/com/example/namwan/firebaseauthfacebooklogin/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -15,6 +16,8 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,6 +27,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -48,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
 
         loginButton = (LoginButton) findViewById(R.id.loginButton);
 
-        loginButton.setReadPermissions(Arrays.asList("email"));
+        loginButton.setReadPermissions(Arrays.asList( "email"));
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -64,6 +69,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onError(FacebookException error) {
                 Toast.makeText(getApplicationContext(), R.string.error_login, Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
             }
         });
 
--- app/src/main/java/com/example/namwan/firebaseauthfacebooklogin/MainActivity.java
+++ app/src/main/java/com/example/namwan/firebaseauthfacebooklogin/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.namwan.firebaseauthfacebooklogin;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView nameTextView;
     private TextView emailTextView;
     private TextView uidTextView;
+    private ImageView profilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +32,36 @@ public class MainActivity extends AppCompatActivity {
         nameTextView = (TextView) findViewById(R.id.nameTextView);
         emailTextView = (TextView) findViewById(R.id.emailTextView);
         uidTextView = (TextView) findViewById(R.id.uidTextView);
+        profilePicture = (ImageView) findViewById(R.id.profilePicture);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         if(user != null){
+            // User is signed in to Firebase, but we can only get
+            // basic info like name, email, and profile photo url
             String name = user.getDisplayName();
             String email = user.getEmail();
             String uid = user.getUid();
+            String photoUrl = user.getPhotoUrl().toString();
 
+            //Log.d("photoUrlTag",photoUrl);
+
+            /*
+            // Even a user's provider-specific profile information
+            // only reveals basic information
+            for (UserInfo profile : user.getProviderData()) {
+                // Id of the provider (ex: google.com)
+                String providerId = profile.getProviderId();
+                // UID specific to the provider
+                String profileUid = profile.getUid();
+                // Name, email address, and profile photo Url
+                String profileDisplayName = profile.getDisplayName();
+                String profileEmail = profile.getEmail();
+                Uri profilePhotoUrl = profile.getPhotoUrl();
+            }
+            */
+
+            Picasso.with(this).load(photoUrl).into(profilePicture);
             nameTextView.setText(name);
             emailTextView.setText(email);
             uidTextView.setText(uid);
